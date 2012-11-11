ea48f26677830f017014b336c2c5bfb63869efea
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-11-11 09:11:02 -0600

    Changed from Activity to SherlockActivity.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
@@ -1,9 +1,11 @@
 package com.animatedjuzz.zebraviews;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ScannerActivity extends Activity {
+public class ScannerActivity extends SherlockActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
