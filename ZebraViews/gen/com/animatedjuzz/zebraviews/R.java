6b81dd5fb84a6d5a6d8e09bce6201aa5eb9c54ef
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-11-11 19:17:48 -0600

    Renamed layout files.

--- ZebraViews/gen/com/animatedjuzz/zebraviews/R.java
+++ ZebraViews/gen/com/animatedjuzz/zebraviews/R.java
@@ -999,9 +999,10 @@ containing a value of this type.
         public static final int abs__search_dropdown_item_icons_2line=0x7f030014;
         public static final int abs__search_view=0x7f030015;
         public static final int abs__simple_dropdown_hint=0x7f030016;
-        public static final int main=0x7f030017;
-        public static final int sherlock_spinner_dropdown_item=0x7f030018;
-        public static final int sherlock_spinner_item=0x7f030019;
+        public static final int scanner_screen_layout=0x7f030017;
+        public static final int scanner_search=0x7f030018;
+        public static final int sherlock_spinner_dropdown_item=0x7f030019;
+        public static final int sherlock_spinner_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
--- ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
@@ -10,6 +10,6 @@ public class ScannerActivity extends SherlockActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.scanner_screen_layout);
     }
 }
