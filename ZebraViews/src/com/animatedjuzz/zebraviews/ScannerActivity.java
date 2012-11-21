4535a3d43bfb1e662d6eb8381162f5df6f1bd7c3
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-11-20 18:24:02 -0600

    Searchbar implementation. Also, revised how ActionBarSherlock is linked to the project.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
@@ -1,9 +1,12 @@
 package com.animatedjuzz.zebraviews;
 
-import com.actionbarsherlock.app.SherlockActivity;
-
-import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.widget.SearchView;
 
 public class ScannerActivity extends SherlockActivity {
     /** Called when the activity is first created. */
@@ -11,5 +14,16 @@ public class ScannerActivity extends SherlockActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scanner_screen_layout);
+    }
+    
+    @Override 
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	com.actionbarsherlock.view.MenuInflater inflater = getSupportMenuInflater();
+    	inflater.inflate(R.layout.scanner_search, menu);
+    	SearchView search = (SearchView) menu.findItem(R.id.scanner_search).getActionView();
+    	//SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    	//search.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    	//search.setIconifiedByDefault(false);
+    	return super.onCreateOptionsMenu(menu);
     }
 }
