2020c6696e5b7eb675073c687f802b9fac89ccb8
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-11-20 20:05:25 -0600

    Added hint text for searching former queries.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ScannerActivity.java
@@ -1,9 +1,8 @@
 package com.animatedjuzz.zebraviews;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.widget.SearchView;
@@ -20,10 +19,10 @@ public class ScannerActivity extends SherlockActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
     	com.actionbarsherlock.view.MenuInflater inflater = getSupportMenuInflater();
     	inflater.inflate(R.layout.scanner_search, menu);
-    	SearchView search = (SearchView) menu.findItem(R.id.scanner_search).getActionView();
-    	//SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-    	//search.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-    	//search.setIconifiedByDefault(false);
+    	SearchView search = 
+    			(SearchView) menu.findItem(R.id.scanner_search).getActionView();
+    	search.setQueryHint(getResources().getString(R.string.scanner_menu_search));
+    	search.setIconifiedByDefault(false);
     	return super.onCreateOptionsMenu(menu);
     }
 }
