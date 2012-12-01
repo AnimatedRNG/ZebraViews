11a6408b69cbef49c644fad614b6c1bbe02112d2
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-11-30 19:20:34 -0600

    Setup back keypress to link back to the scanner activity. The scanner activity destroys itself when it is done (need to due to a glitch in ZXing), so this is the only way to go back so far.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsActivity.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsActivity.java
@@ -1,7 +1,22 @@
 package com.animatedjuzz.zebraviews;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class ReviewsActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+        Intent scanner = new Intent(ReviewsActivity.this, ScannerActivity.class);
+        startActivity(scanner);
+	}
 
 }
