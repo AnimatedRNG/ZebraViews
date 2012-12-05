f273cc82046a4b473cbb9ed5a3afe13afec2898b
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-12-04 18:42:36 -0600

    Renamed to SherlockFragment.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsTabListener.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsTabListener.java
@@ -1,12 +1,26 @@
 package com.animatedjuzz.zebraviews;
 
+import android.app.Activity;
 import android.support.v4.app.FragmentTransaction;
 
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragment;
 
 public class ReviewsTabListener implements TabListener {
 
+	SherlockActivity activity;
+	private SherlockFragment mFragment;
+    private final Activity mActivity;
+    private final String mTag;
+    private final Class<T> mClass;
+	
+	
+	public ReviewsTabListener(SherlockActivity activity, String tag, Class<T> clz) {
+		
+	}
+	
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
 		
