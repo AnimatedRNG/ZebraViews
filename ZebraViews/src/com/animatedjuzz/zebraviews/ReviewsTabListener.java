e3f91730b7d7ab91c67e3acd3462fc3124e97620
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-12-04 18:38:41 -0600

    Initial commit of ReviewsTabListener which will be the TabListener for the ReviewsActivity.

---
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsTabListener.java
@@ -0,0 +1,25 @@
+package com.animatedjuzz.zebraviews;
+
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+
+public class ReviewsTabListener implements TabListener {
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		
+	}
+
+}
