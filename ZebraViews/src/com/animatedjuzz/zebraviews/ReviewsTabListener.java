5ccae2e4dfe31c052f76349a5eb98af967f75d1d
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-12-04 18:54:53 -0600

    Pretty much the standard code on the Android Dev website, except using ABS.

--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsTabListener.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsTabListener.java
@@ -1,6 +1,5 @@
 package com.animatedjuzz.zebraviews;
 
-import android.app.Activity;
 import android.support.v4.app.FragmentTransaction;
 
 import com.actionbarsherlock.app.ActionBar.Tab;
@@ -8,32 +7,41 @@ import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class ReviewsTabListener implements TabListener {
+public class ReviewsTabListener<T extends SherlockFragment> implements TabListener {
 
-	SherlockActivity activity;
 	private SherlockFragment mFragment;
-    private final Activity mActivity;
-    private final String mTag;
-    private final Class<T> mClass;
+    private SherlockActivity mActivity;
+    private String mTag;
+    private Class<T> mClass;
 	
 	
-	public ReviewsTabListener(SherlockActivity activity, String tag, Class<T> clz) {
-		
+    public ReviewsTabListener(SherlockActivity activity, String tag, Class<T> clz) {
+    	mActivity = activity;
+        mTag = tag;
+        mClass = clz;
 	}
 	
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		
+        if (mFragment == null) {
+            mFragment =  //Um... not sure if this is okay....
+            		(SherlockFragment) SherlockFragment.instantiate
+            		(mActivity, mClass.getName());
+            ft.add(android.R.id.content, mFragment, mTag);
+        } else {
+            ft.attach(mFragment);
+        }
 	}
 
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		
+		if (mFragment != null) {
+            ft.detach(mFragment);
+        }
 	}
 
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		
 	}
 
 }
