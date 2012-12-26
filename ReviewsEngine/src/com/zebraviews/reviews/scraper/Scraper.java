4c653f4cc638f3ca5ae7dd5973a337c0889522bc
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-12-26 12:27:07 -0600

    Made a new package for all the scrapers.

--- ReviewsEngine/src/com/zebraviews/reviews/scraper/Scraper.java
+++ ReviewsEngine/src/com/zebraviews/reviews/scraper/Scraper.java
@@ -15,7 +15,7 @@
 //	You should have received a copy of the GNU General Public License
 //	along with ZebraViews.  If not, see <http://www.gnu.org/licenses/>.
 
-package com.zebraviews.reviews;
+package com.zebraviews.reviews.scraper;
 
 public interface Scraper extends Runnable{
 
