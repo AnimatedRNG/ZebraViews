fdcad8b1f164bc22c4b1dde37256d06b7b57d1a5
AnimatedJuzz <kazasrinivas3@gmail.com>
2012-12-31 12:57:57 -0600

    Added some basic functionality so that it can at least run the scraper. For now interrupts will be handled in the Scraper itself.

--- ReviewsEngine/src/com/zebraviews/reviews/ReviewFetchThread.java
+++ ReviewsEngine/src/com/zebraviews/reviews/ReviewFetchThread.java
@@ -17,6 +17,40 @@
 
 package com.zebraviews.reviews;
 
+import com.zebraviews.reviews.scraper.Scraper;
+
 public class ReviewFetchThread extends Thread {
 
+	private Scraper scraper;
+	private boolean running;
+	
+	public ReviewFetchThread() { }
+	
+	public ReviewFetchThread(Scraper scraper) {
+		this.scraper = scraper;
+	}
+	
+	public boolean activate() {
+		if (this.isActive())
+			return false;
+		else
+		{
+			scraper.run();
+			return true;
+		}
+	}
+	
+	public boolean init(Scraper scraper) {
+		if (this.isActive())
+			return false;
+		else
+		{
+			this.scraper = scraper;
+			return true;
+		}
+	}
+	
+	public boolean isActive() {
+		return running;
+	}
 }
