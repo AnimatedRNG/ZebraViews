cd2a861631ee4f1b02809929c507ec3d35fbcd3c
AnimatedJuzz <kazasrinivas3@gmail.com>
2013-01-29 20:56:21 -0600

    Added code to open up the priority list XML file for reading.

--- ReviewsEngine/src/com/zebraviews/reviews/PriorityList.java
+++ ReviewsEngine/src/com/zebraviews/reviews/PriorityList.java
@@ -17,6 +17,46 @@
 
 package com.zebraviews.reviews;
 
-public class PriorityList {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.zebraviews.reviews.scraper.Scraper;
+
+public class PriorityList {
+	
+	private ArrayList<Scraper> scraperPool;
+	
+	private void inflateList() {
+		scraperPool = new ArrayList<Scraper>();
+		DocumentBuilderFactory priorityFactory = 
+				DocumentBuilderFactory.newInstance();
+		DocumentBuilder priorityBuilder = null;
+		try {
+			priorityBuilder = priorityFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			System.out.println("Parser Exception");
+			// TODO: Add error handling...
+		}
+		Document priority = null;
+		try {
+			priority = priorityBuilder.parse
+					(new File("XML/priority_list.xml"));
+		} catch (SAXException e) {
+			System.out.println("SAX Exception");
+			// TODO Add error handling...
+		} catch (IOException e) {
+			System.out.println("IO Exception");
+			// TODO Add error handling...
+		}
+		priority.getDocumentElement().normalize();
+	}
 }
