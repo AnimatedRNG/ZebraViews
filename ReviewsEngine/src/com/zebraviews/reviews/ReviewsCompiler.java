10e09478337f1adfbfd9767c93de41a9b8f8ecfc
AnimatedJuzz <kazasrinivas3@gmail.com>
2013-03-28 20:59:23 -0500

    First release of thread-juggling code. Apt to change

--- ReviewsEngine/src/com/zebraviews/reviews/ReviewsCompiler.java
+++ ReviewsEngine/src/com/zebraviews/reviews/ReviewsCompiler.java
@@ -17,6 +17,41 @@
 
 package com.zebraviews.reviews;
 
-public class ReviewsCompiler {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReviewsCompiler {
+	
+	public final static int NUMBER_OF_THREADS = 8;
+	public final static long POLL_TIME = 100;
+	
+	private List<ReviewFetchThread> fetchers;
+	private PriorityList priorityList;
+	
+	public ReviewsCompiler() {
+		fetchers = new ArrayList<ReviewFetchThread>();
+		priorityList = new PriorityList();
+		
+		for (int i = 0; i < 8; i++)
+			fetchers.add(new ReviewFetchThread(priorityList.getScraper()));
+	}
+	
+	public void activateAll() {
+		for (ReviewFetchThread r : fetchers)
+			r.activate();
+	}
+	
+	public ReviewsData executePartially() throws InterruptedException {
+		ReviewsData reviews = new ReviewsData();
+		for (ReviewFetchThread r : fetchers)
+		{
+			while (r.hasReviews())
+				reviews.addReview(r.retrieveReview());
+			
+			if (!r.isAlive() && priorityList.hasScraper())
+				r.init(priorityList.getScraper());
+		}
+		Thread.sleep(ReviewsCompiler.POLL_TIME);
+		return reviews;
+	}
 }
