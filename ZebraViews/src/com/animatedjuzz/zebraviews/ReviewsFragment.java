38ab63e41a6a7faf26c251b7e597a1a80cb6504f
AnimatedJuzz <kazasrinivas3@gmail.com>
2013-06-23 19:30:59 -0500

    Changed permissions and connected ReviewsEngine with ZebraViews

--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsFragment.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsFragment.java
@@ -17,13 +17,16 @@
 
 package com.animatedjuzz.zebraviews;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -40,6 +43,21 @@ public class ReviewsFragment extends SherlockFragment implements ReviewsListener
 	
 	private final static int MINIMUM_REVIEWS = 8;
 	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		String upc = this.getActivity().getIntent().
+				getStringExtra("com.animated.juzzz.zebraviews.BARCODE_TEXT");
+		ReviewsManager downloader = null;
+		try {
+			downloader = new ReviewsManager(this, upc,
+					this.getActivity().getAssets().open("XML/priority_list.xml"));
+		} catch (IOException e) {
+			Log.d("Priority Inflation", "No priority list found");
+		}
+		downloader.execute();
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -62,16 +80,22 @@ public class ReviewsFragment extends SherlockFragment implements ReviewsListener
 	public void onReviewsDataDownloaded(ReviewsData data) {
 		for (Review review : data.getReviews())
 			this.reviewList.add(review.getReview());
-		if (this.reviewList.size() >= ReviewsFragment.MINIMUM_REVIEWS)
-		{
-			this.adapter.notifyDataSetChanged();
+		if (this.reviewList.size() == ReviewsFragment.MINIMUM_REVIEWS)
 			this.progress.setVisibility(View.GONE);
-		}
+		if (this.reviewList.size() >= ReviewsFragment.MINIMUM_REVIEWS)
+			this.updateList();
 	}
 
 	@Override
 	public void onCompletion() {
-		this.adapter.notifyDataSetChanged();
+		this.progress.setVisibility(View.GONE);
+		this.updateList();
+	}
+	
+	private void updateList() {
+		BaseAdapter adapter = ((BaseAdapter) ((ListView)getView().
+				findViewById(R.id.review_list)).getAdapter());
+		adapter.notifyDataSetChanged();
 	}
 	
 }
--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsManager.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsManager.java
@@ -1,5 +1,53 @@
 package com.animatedjuzz.zebraviews;
 
-public class ReviewsManager {
+import java.io.InputStream;
 
+import android.os.AsyncTask;
+
+import com.zebraviews.reviews.ReviewsCompiler;
+import com.zebraviews.reviews.ReviewsData;
+
+public class ReviewsManager extends AsyncTask<Void, ReviewsData, Void> {
+
+	private ReviewsListener gui;
+	private String upc;
+	private ReviewsCompiler compiler;
+	
+	public ReviewsManager(ReviewsListener gui, String upc, InputStream 
+			priorityListXML) {
+		this.gui = gui; 
+		this.upc = upc;
+		this.compiler = new ReviewsCompiler(upc, priorityListXML);
+	}
+	
+	@Override
+    protected void onPreExecute() {
+		this.compiler.activateAll();
+	}
+
+	@Override
+	protected Void doInBackground(Void... arg0) {
+		do
+		{
+			try {
+				publishProgress(this.compiler.executePartially());
+			} catch (InterruptedException e) {
+				// Bad internet connection or some problem.
+				// Can just skip the review
+			}
+		} while (!compiler.isComplete());
+		
+		return null;
+	}
+	
+	@Override
+	protected void onProgressUpdate(ReviewsData... progress) {
+        for (ReviewsData r : progress)
+        	this.gui.onReviewsDataDownloaded(r);
+    }
+
+	@Override
+	protected void onPostExecute(Void result) {
+		this.gui.onCompletion();
+	}
 }
