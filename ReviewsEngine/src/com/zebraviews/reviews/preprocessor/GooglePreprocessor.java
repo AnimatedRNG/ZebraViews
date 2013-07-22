7941fe15081cd506802f22e0448a64ec3e973675
AnimatedJuzz <kazasrinivas3@gmail.com>
2013-07-22 11:49:08 -0500

    Added price and description capacities to the GooglePreprocessor

--- ReviewsEngine/src/com/zebraviews/reviews/preprocessor/GooglePreprocessor.java
+++ ReviewsEngine/src/com/zebraviews/reviews/preprocessor/GooglePreprocessor.java
@@ -1,11 +1,18 @@
 package com.zebraviews.reviews.preprocessor;
 
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
+import com.zebraviews.reviews.JSONRequest;
+
 public class GooglePreprocessor extends Preprocessor {
 
 	private final static String searchString = "https://www.googleapis.com/" +
 			"shopping/search/v1/public/products?key=" +
 			"AIzaSyDFLBwQFNNxWXkfeJ_sU30XRrdroRmh6TY&country=US" +
 			"&restrictBy=gtin:";
+	
+	protected final static String DATA_NAME = "Google";
 	
 	@Override
 	public void onSimultaneousExecute() {
@@ -14,12 +21,42 @@ public class GooglePreprocessor extends Preprocessor {
 
 	@Override
 	public void onPreExecute() {
-
+		JSONArray items = (JSONArray) JSONRequest.getRequest(searchString + 
+				this.getFetchThread().getCompiler().getUPC()).get("items");
+		
+		if (items == null || items.size() == 0)
+			return;
+		
+		String title = (String) ((JSONObject) ((JSONObject) items.get(0)).
+				get("product")).get("title");
+		String prices = new String();
+		String description = new String();
+		
+		for (int itemIndex = 0; itemIndex < items.size(); itemIndex++)
+		{
+			JSONArray inventories = (JSONArray) ((JSONObject) 
+					((JSONObject) items.get(itemIndex)).get("product"))
+					.get("inventories");
+			prices += ((JSONObject) inventories.get(0)).get("price") + " ";
+			description += ((JSONObject)((JSONObject) items.get(itemIndex)).
+					get("product")).get("description")
+					+ GooglePreprocessor.DELIMITER;
+		}
+		
+		this.getPreprocessingData().put("title", title);
+		this.getPreprocessingData().put("prices", prices);
+		this.getPreprocessingData().put("description", description);
+		this.done();
 	}
 
 	@Override
 	public String getPreprocessingDataName() {
-		return null;
+		return GooglePreprocessor.DATA_NAME;
 	}
 
+	public static void main(String[] args) { 
+		GooglePreprocessor goog = new GooglePreprocessor();
+		goog.init(DATA_NAME);
+		goog.onPreExecute();
+	}
 }
