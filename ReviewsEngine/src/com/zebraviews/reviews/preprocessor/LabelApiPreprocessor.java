1f29e500e0b9a3ac3c1b2683cf63b9718a071c56
Satya Krishnan <satyajitkrishnan@gmail.com>
2013-07-22 18:08:43 -0500

    Added exception handling for labelAPI preprocessor

--- ReviewsEngine/src/com/zebraviews/reviews/preprocessor/LabelApiPreprocessor.java
+++ ReviewsEngine/src/com/zebraviews/reviews/preprocessor/LabelApiPreprocessor.java
@@ -18,37 +18,48 @@ public class LabelApiPreprocessor extends Preprocessor {
 
 	@Override
 	public void onPreExecute() {
-		String product = new String();
-		String upc = this.getFetchThread().getCompiler().getUPC();
-		LabelApiURI requestGenerator = 
-				new LabelApiURI(upc, API_KEY);
-		String sessionID = (String) 
-				JSONRequest.
-				getRequest(requestGenerator.getCreateSessionURI()).
-				get("session_id");
-		JSONRequest.postRequestWithoutStructure(requestGenerator.
-				getSetProfileURI(sessionID, true, true, true, true, true,
-						true, true, true, true, true, true, true ,true,
-						true, true));
-		JSONArray results = (JSONArray) JSONRequest.
-				getRequest(requestGenerator.
-						getLabelArrayURI(sessionID, "1", "0")).
-						get("productsArray");
-		product = (((JSONObject) results.get(0)).
-				get("product_name")).toString();
-		String allergens = "";
-		JSONArray allergenResults = ((JSONArray)
-				((JSONObject) results.get(0)).get("allergens"));
-		for (int j = 0; j < 15; j++) {
-			if(((JSONObject) allergenResults.get(j)).
-					get("allergen_value").equals("2")) {
-				allergens += (((String)((JSONObject) allergenResults.
-						get(j)).get("allergen_name")).replace(" ", "_")) + " ";
+		try
+		{
+			String product = new String();
+			String upc = this.getFetchThread().getCompiler().getUPC();
+			LabelApiURI requestGenerator = 
+					new LabelApiURI(upc, API_KEY);
+			if(JSONRequest.getRequest(requestGenerator.getCreateSessionURI())==null)
+				return;
+			String sessionID = (String) 
+					JSONRequest.
+					getRequest(requestGenerator.getCreateSessionURI()).
+					get("session_id");
+			JSONRequest.postRequestWithoutStructure(requestGenerator.
+					getSetProfileURI(sessionID, true, true, true, true, true,
+							true, true, true, true, true, true, true ,true,
+							true, true));
+			if(JSONRequest.getRequest(requestGenerator.getLabelArrayURI(sessionID, "1", "0"))==null)
+				return;
+			JSONArray results = (JSONArray) JSONRequest.
+					getRequest(requestGenerator.
+							getLabelArrayURI(sessionID, "1", "0")).
+							get("productsArray");
+			product = (((JSONObject) results.get(0)).
+					get("product_name")).toString();
+			String allergens = "";
+			JSONArray allergenResults = ((JSONArray)
+					((JSONObject) results.get(0)).get("allergens"));
+			for (int j = 0; j < 15; j++) {
+				if(((JSONObject) allergenResults.get(j)).
+						get("allergen_value").equals("2")) {
+					allergens += (((String)((JSONObject) allergenResults.
+							get(j)).get("allergen_name")).replace(" ", "_")) + " ";
+				}
 			}
+			this.getPreprocessingData().put("allergens", allergens);
+			this.getPreprocessingData().put("title", product);
+			this.done();
 		}
-		this.getPreprocessingData().put("allergens", allergens);
-		this.getPreprocessingData().put("title", product);
-		this.done();
+		catch(Exception e)
+		{
+			return;
+		}
 	}
 
 	@Override
