e3f0db9c446f538fa6457f18f51b7fbb3f63172e
AnimatedJuzz <kazasrinivas3@gmail.com>
2013-07-22 14:13:03 -0500

    Added preliminary support for PreprocesssingData

--- ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsManager.java
+++ ZebraViews/src/com/animatedjuzz/zebraviews/ReviewsManager.java
@@ -1,34 +1,23 @@
 package com.animatedjuzz.zebraviews;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
-import net.minidev.json.parser.ParseException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import android.os.AsyncTask;
 
 import com.zebraviews.reviews.ReviewsCompiler;
 import com.zebraviews.reviews.ReviewsData;
+import com.zebraviews.reviews.preprocessor.PreprocessingData;
 
 public class ReviewsManager extends AsyncTask<Void, ReviewsData, Void> {
 
 	private ReviewsListener gui;
 	private static String upc;
 	private ReviewsCompiler compiler;
+	private ArrayList<PreprocessingData> preprocessedData;
 	
 	private final static String searchString = "https://www.googleapis.com/" +
 			"shopping/search/v1/public/products?key=" +
@@ -52,7 +41,8 @@ public class ReviewsManager extends AsyncTask<Void, ReviewsData, Void> {
 		do
 		{
 			try {
-				publishProgress(this.compiler.executePartially());
+				if (!this.compiler.isPrexecuting())
+					publishProgress(this.compiler.executePartially());
 			} catch (InterruptedException e) {
 				// Bad internet connection or some problem.
 				// Can just skip the review
@@ -71,54 +61,46 @@ public class ReviewsManager extends AsyncTask<Void, ReviewsData, Void> {
 	@Override
 	protected void onPostExecute(Void result) {
 		this.gui.onCompletion();
+		this.preprocessedData = this.compiler.retrievePreprocessingData();
 	}
 	
+	// Preferring shortest name
 	public String getProductName() {
-		ProductNameRetriever retriever = new ProductNameRetriever();
-		retriever.execute();
-		return retriever.title;
+		
+		String labelAPI = this.getPreprocessedData("LabelAPI").get("title");
+		if (labelAPI != null)
+			return labelAPI;
+		
+		//String amazon = this.getPreprocessedData("Amazon").get("title");
+		String google = this.getPreprocessedData("Google").get("title");
+		
+		//return (amazon.length() < google.length()) ? amazon : google;
+		return google;
 	}
 	
-	private class ProductNameRetriever extends AsyncTask<Void, Void, Void> {
-
-		protected String title;
+	public String getProductDescription() {
+		return this.getPreprocessedData("Amazon").get("description");
+	}
+	
+	public String getBestPrice() {
+		List<String> prices =
+				Arrays.asList(this.getPreprocessedData("Google").get("price").
+				split("\\s+"));
 		
-		@Override
-		protected Void doInBackground(Void... params) {
-			DefaultHttpClient httpclient = 
-					new DefaultHttpClient(new BasicHttpParams());
-			HttpGet request = new HttpGet();
-			InputStream inputStream = null;
-			try {
-				request.setURI(new URI(ReviewsManager.searchString
-						+ ReviewsManager.upc));
-				HttpResponse response = httpclient.execute(request);
-				inputStream = response.getEntity().getContent();
-			} catch (URISyntaxException e1) {
-				this.title = null;
-				return null;
-			} catch (IllegalStateException e) {
-				this.title = null;
-				return null;
-			} catch (IOException e) {
-				this.title = null;
-				return null;
-			}
-			
-			JSONObject titlePage = null;
-			try {
-				titlePage = (JSONObject) JSONValue.parseWithException(inputStream);
-			} catch (IOException e) {
-				this.title = null;
-				return null;
-			} catch (ParseException e) {
-				this.title = null;
-				return null;
-			}
-			JSONArray items = (JSONArray) titlePage.get("items");
-			JSONObject titleObj = (JSONObject) items.get(0);
-			this.title = (String) titleObj.get("title");
-			return null;
+		double bestPrice = Double.MAX_VALUE;
+		
+		for (String price : prices)
+		{
+			double priceVal = Double.parseDouble(price);
+			if (priceVal < bestPrice)
+				bestPrice = priceVal;
 		}
+		
+		return bestPrice + "";
+	}
+	
+	public PreprocessingData getPreprocessedData(String name) {
+		return this.preprocessedData.get(Collections.binarySearch(
+				this.preprocessedData, new PreprocessingData(name)));
 	}
 }
